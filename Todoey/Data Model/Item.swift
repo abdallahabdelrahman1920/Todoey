ca614d13c03104931134713432b167fa293b8d2e
Abdallah Ismail <abdallahismail@Abdallahs-MacBook-Pro.local>
2019-05-19 11:12:29 +0200

    Commit to MVC design pattern.

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,16 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Abdallah Ismail on 5/19/19.
+//  Copyright © 2019 Abdallah Abdelrahman. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    
+    var title : String = ""
+    
+    var done : Bool = false
+}
